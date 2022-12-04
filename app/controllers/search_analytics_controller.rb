88017d32c0e98994f69b54033cb885f99917d2d7
ksigei <sigeikiprono4@gmail.com>
2022-12-05 02:28:40 +0300

    Seach analytics index

--- app/controllers/search_analytics_controller.rb
+++ app/controllers/search_analytics_controller.rb
@@ -1,10 +1,10 @@
 class SearchAnalyticsController < ApplicationController
   def index
-    @search_analytics = SearchAnalytics.all.order(count: :desc)
+    @search_analytics = SearchAnalytic.all.order(count: :desc)
   end
 
   def update
-    @search_analytics = SearchAnalytics.find_by(query: params[:query])
+    @search_analytics = SearchAnalytic.find_by(query: params[:query])
     @search_analytics.count += 1
     @search_analytics.save
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'search_analytics/index'
-  get 'searches/create'
-  devise_for :users
+  # get 'search_analytic/index'
+  # get 'searches/create'
+  # root 'search_analytics#index'
+  root 'search_analytics#index'
+  resources :searches
+  resources :search_analytic
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
