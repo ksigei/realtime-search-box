f4c0401684f125d1304e748be07c0dc8023a103c
ksigei <sigeikiprono4@gmail.com>
2022-12-05 02:12:55 +0300

    Search analytics controller update

--- app/controllers/search_analytics_controller.rb
+++ app/controllers/search_analytics_controller.rb
@@ -2,4 +2,10 @@ class SearchAnalyticsController < ApplicationController
   def index
     @search_analytics = SearchAnalytics.all.order(count: :desc)
   end
+
+  def update
+    @search_analytics = SearchAnalytics.find_by(query: params[:query])
+    @search_analytics.count += 1
+    @search_analytics.save
+  end
 end
